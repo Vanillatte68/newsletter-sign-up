98305670555309c168fb340568b8979378333c4a
Vanillatte68 <roselinyosefa@yahoo.com>
2024-06-13 15:47:32 +0700

    commit

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+// get variable
 var errorMsg = document.getElementById("errorMsg");
 var emailValue = document.getElementById("emailValue");
+var mainForm = document.getElementById("mainForm");
+var successForm = document.getElementById("successForm");
+var form = document.querySelector("form");
+
+// validate form
 function validateForm() {
   let email = document.getElementById("email");
 
   // if email empty
   if (!email.value.match(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/)) {
     errorMsg.style.display = "block";
-    email.style.borderColor = "var(--primary-color)";
+    email.classList.add("inavlid");
     // if email valid
   } else {
     errorMsg.style.display = "none";
-    email.style.borderColor = "var(--grey)";
+    email.classList.remove("inavlid");
     emailValue.innerHTML = email.value;
+    form.reset();
+    mainForm.style.display = "none";
+    successForm.style.display = "flex";
   }
+}
+
+function dismissForm() {
+  successForm.style.display = "none";
+  mainForm.style.display = "flex";
 }
